e7ea9e446fecbd529471e183c26a509d9e8c863d
Rahul Maurya <rahulmaurya7279@gmail.com>
2024-10-26 13:26:37 +0530

    Add files via upload

---
+++ PW6/iter.js
@@ -0,0 +1,9 @@
+const originalNumbers = [2, 5, 8, 10, 3];
+
+const doubledNumbers = [];
+
+originalNumbers.forEach((number) => {
+  doubledNumbers.push(number * 2);
+});
+
+console.log(doubledNumbers);
